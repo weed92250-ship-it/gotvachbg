741a4645dfce8686b30a82244ad83c6d4d03d513
weed92250-ship-it <weed92250@gmail.com>
2026-09-26 12:53:52 +0300

    Поправяме sitemap с реалните URL адреси

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -1,18 +1,11 @@
-const SITE_URL = 'https://gotvachbg.pages.dev';
-
-export async function onRequestGet({ env }) {
-  const { results } = await env.DB.prepare('SELECT id, date FROM recipes ORDER BY date DESC').all();
-  const urls = results
-    .map(r => `  <url>\n    <loc>${SITE_URL}/recipe/${r.id}</loc>\n    <lastmod>${r.date}</lastmod>\n  </url>`)
-    .join('\n');
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>${SITE_URL}/</loc>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>
-${urls}
-</urlset>`;
-  return new Response(xml, { headers: { 'content-type': 'application/xml; charset=UTF-8' } });
-}
+const SITE_URL='https://gotvachbg.pages.dev';
+const pages=['/','/categories.html','/about.html','/contacts.html','/privacy.html'];
+const recipes=[
+'/recipes/kralski-svinski-dzholan-na-baven-ogan-s-tamna-bira-rozmarin-i-karamelizirani-korenoplodni.html',
+'/recipes/klasicheski-hrupkavi-triagalni-banichki-sas-sirene-i-krave-maslo.html',
+'/recipes/klasicheski-hrupkavi-triagalni-banichki-sas-sirene.html',
+'/recipes/krem-supa-ot-tikva.html','/recipes/domashni-mekici.html','/recipes/pecheni-kartofi-s-podpravki.html',
+'/recipes/pileshko-s-oriz-na-furna.html','/recipes/shokoladov-keks.html','/recipes/shopska-salata.html',
+'/recipes/bob-chorba.html','/recipes/kyufteta-na-furna.html'];
+export async function onRequestGet(){const urls=[...pages,...recipes].map(path=>`<url><loc>${SITE_URL}${path}</loc></url>`).join('');
+return new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`,{headers:{'content-type':'application/xml; charset=UTF-8'}});}
